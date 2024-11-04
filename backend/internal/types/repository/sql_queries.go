b90f82521d4de8fb66337969abaa0894b4fb37fc
Jason Ngu <jasonaqw@gmail.com>
2024-11-04 15:21:39 -0800

    Updated query

--- backend/internal/types/repository/sql_queries.go
+++ backend/internal/types/repository/sql_queries.go
@@ -4,5 +4,5 @@ const (
 	createTypeQuery       = `INSERT INTO public.types (type_name) VALUES ($1) RETURN *`
 	getTypeById           = `SELECT * FROM public.types WHERE type_id = $1`
 	getAllTypes           = `SELECT * FROM public.types`
-	getTotalCountAllTypes = `SELECT COUNT(type_id) FROM public.types`
+	getTotalCountAllTypes = `SELECT COUNT(*) FROM public.types`
 )
