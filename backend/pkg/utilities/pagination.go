e3b96d74dbf3a875d807368fc9cbf3ce54132d33
Jason Ngu <jasonaqw@gmail.com>
2024-11-04 15:03:14 -0800

    Added pagination utility

---
+++ backend/pkg/utilities/pagination.go
@@ -0,0 +1,24 @@
+package utilities
+
+import "math"
+
+const defaultSize = 10
+
+type PaginationQuery struct {
+	Size int `json:"size"`
+	Page int `json:"page"`
+}
+
+func (p *PaginationQuery) GetSize() int {
+	return p.Size
+}
+
+func (p *PaginationQuery) GetPage() int {
+	return p.Page
+}
+
+// Get total pages int
+func GetTotalPages(totalCount int, pageSize int) int {
+	d := float64(totalCount) / float64(pageSize)
+	return int(math.Ceil(d))
+}
